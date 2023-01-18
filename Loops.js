029e459fe50cc974b1932720117b5ac4cc235576
utkarshsingh2122 <107643326+utkarshsingh2122@users.noreply.github.com>
2023-01-18 16:09:25 +0530

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,18 @@
+// While Loop
+let counter = 0;
+while (counter < 10) {
+    document.write("Utkarsh" + "\n");
+    counter++;
+}
+// Do While Loop
+let counter1 = 0
+do {
+    document.write("Utkarsh1" + "\n");
+    counter1++;
+} while (counter1 < 10)
+
+// For Loop
+let counter2 = 0;
+for (counter2; counter2 <= 10; counter2++) {
+    document.write("Utkarsh3" + "\n");
+}

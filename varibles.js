116bce953e49fad5b6a21a0b09c388dbda25d4f4
utkarshsingh2122 <107643326+utkarshsingh2122@users.noreply.github.com>
2023-03-15 21:50:56 +0530

    Add files via upload

---
+++ varibles.js
@@ -0,0 +1,6 @@
+var a = 67 // a contains 67
+console.log(a)
+    //a = "harry"
+    //console.log(a)
+    // let 8utkarsh=7 // Not allowed this will throw an error
+    // let var = 7 // these are reserved keyword

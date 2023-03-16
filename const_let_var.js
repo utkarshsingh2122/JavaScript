94968ff4e7f6798fa87b5cb5c5f86716244f80a4
utkarshsingh2122 <107643326+utkarshsingh2122@users.noreply.github.com>
2023-03-16 22:34:04 +0530

    Add files via upload

---
+++ const_let_var.js
@@ -0,0 +1,14 @@
+var a = 45;
+var b = "Utkarsh";
+var c = null;
+var d = undefined; {
+    let b = 'this'
+    console.log(b)
+}
+console.log(b)
+    // let is basically sugested to use 
+    // let and block is block scoped
+    // let can be updated but not re decleared
+
+const author = "Utkarsh";
+// const cant be and and cant be rede clared

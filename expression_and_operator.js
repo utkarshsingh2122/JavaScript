b44d7f33d1a88f10f2290f337a41d51e839e5315
utkarshsingh2122 <107643326+utkarshsingh2122@users.noreply.github.com>
2023-03-20 23:12:18 +0530

    Add files via upload

---
+++ expression_and_operator.js
@@ -0,0 +1,15 @@
+// Operators in JS
+
+// Arithmetic operators
+let a = 45;
+let b = 4;
+console.log("a + b = ", a + b)
+console.log("a - b = ", a + b)
+console.log("a / b = ", a + b)
+console.log("a ** b = ", a + b)
+console.log("a % b = ", a + b)
+console.log("a + b = ", ++b)
+console.log("a + b = ", a)
+console.log("a-- = ", a--)
+
+// Assignment operators

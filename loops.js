61d0c90dcfe76a7f57a8c953d78cf2ccf99da754
utkarshsingh2122 <107643326+utkarshsingh2122@users.noreply.github.com>
2023-03-27 23:45:41 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,20 @@
+ // For-loop
+
+ for (let i = 0; i < 34; i++) {
+     console.log(i)
+ }
+
+ // for-in loop
+
+ let obj = {
+     utkarsh: 90,
+     shubh: 87,
+     shiv: 45,
+     ritik: 34
+ }
+ for (let a in obj) {
+     console.log("Marks of " + a + " are " + obj[a])
+ }
+
+ // for-of loop
+ //Used in array basically
